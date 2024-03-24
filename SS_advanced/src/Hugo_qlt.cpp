9b379200257c7967332f5ab2e002a20ef4b748d9
thanhbl-hust <lamthanhbui02@gmail.com>
2024-03-24 14:51:17 +0700

    add process

--- SS_advanced/src/Hugo_qlt.cpp
+++ SS_advanced/src/Hugo_qlt.cpp
@@ -1,22 +1,17 @@
 #include<iostream>
 #define maxValue 1000000000;
-
 int result = maxValue;
 int tc;
 int N;
-
-int stt[4] = {-1, -1, -1, -1};
 int coor[4];
-int number[4];
 int sit[65];
+int number[4];
 int visited[4] = {-1, -1, -1, -1};
-
+int stt[4] = {-1, -1, -1, -1};
 using namespace std;
-
 void resetSitdown(){
     for(int i = 1; i < N + 1; i++) sit[i] = -1;
 }
-
 void backtrack(int k, int stepCount){
     if(k == 4){
         if(stepCount < result){
@@ -24,7 +19,6 @@ void backtrack(int k, int stepCount){
             return;
         }
     }
-
     for(int i = 1; i < 4; i++){
         if(visited[i] == -1){
             visited[i] = 1;
@@ -32,8 +26,6 @@ void backtrack(int k, int stepCount){
             int bonusStep = 0;
             int toado = coor[i]; // toa do cua cong;
             int num = number[i]; // so nguoi o cong do;
-
-
             //add num - 1 thang vao truoc;
             for(int j = 1; j < num; j++){
                 int nearLeft = 0, nearRight = 0;
@@ -49,30 +41,23 @@ void backtrack(int k, int stepCount){
                     bonusStep += nearRight + 1;
                 }
             }
-
-
             int nearL = 0, nearR = 0;
             while(sit[toado - nearL] != -1) nearL += 1;
             while(sit[toado + nearR] != -1) nearR += 1;
             if(toado - nearL < 1) nearL = maxValue;
             if(toado + nearR > N) nearR = maxValue;
-
             if(nearL < nearR){
-
                 sit[toado - nearL] = i;
                 bonusStep += nearL + 1;
                 backtrack(k + 1, stepCount + bonusStep);
                 sit[toado - nearL] = -1;
                 bonusStep -= nearL + 1;
-
             }else if(nearL > nearR){
-
                 sit[toado + nearR] = i;
                 bonusStep += nearR + 1;
                 backtrack(k + 1, stepCount + bonusStep);
                 sit[toado + nearR] = -1;
                 bonusStep -= nearR + 1;
-
             }else{
                 //...??
                 sit[toado - nearL] = i;
@@ -80,31 +65,23 @@ void backtrack(int k, int stepCount){
                 backtrack(k + 1, stepCount + bonusStep);
                 sit[toado - nearL] = -1;
                 bonusStep -= nearL + 1;
-
-
                 sit[toado + nearR] = i;
                 bonusStep += nearR + 1;
                 backtrack(k + 1, stepCount + bonusStep);
                 sit[toado + nearR] = -1;
                 bonusStep -= nearR + 1;
             }
-
             for(int j = 1; j < N + 1; j++){
                 if(sit[j] == i) sit[j] = -1;
             }
-
             visited[i] = -1;
             stt[k] = -1;
         }
     }
 }
-
 int main(){
-
     freopen("inp.txt", "r", stdin);
-
     int test; cin >> test;
-
     for(int tc = 0; tc < test; tc++){
         cin >> N;
         result = maxValue;
@@ -113,6 +90,5 @@ int main(){
         backtrack(1, 0);
         cout << "Case #" << tc + 1 << endl << result << endl;
     }
-
     return 0;
 }
